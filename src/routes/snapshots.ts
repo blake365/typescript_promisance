f2b0a31e4e1f3023e125057084d292908524a496
blake365 <68243611+blake365@users.noreply.github.com>
2024-01-15 21:44:59 -0500

    Add json2csv library and implement downloadSnapshots endpoint

--- src/routes/snapshots.ts
+++ src/routes/snapshots.ts
@@ -6,6 +6,8 @@ import auth from '../middleware/auth'
 import user from '../middleware/user'
 import User from '../entity/User'
 import { Raw } from 'typeorm'
+import { Parser } from 'json2csv'
+import { Readable } from 'stream'
 
 // READ
 const getSnapshot = async (req: Request, res: Response) => {
@@ -68,7 +70,7 @@ const paginateSnapshot = async (req: Request, res: Response) => {
 	// 	return res.status(500).json({ error: 'Empire ID mismatch' })
 	// }
 	take = parseInt(take)
-	console.log(page, take)
+	// console.log(page, take)
 	try {
 		let snapshot = await EmpireSnapshot.find({
 			where: {
@@ -89,10 +91,51 @@ const paginateSnapshot = async (req: Request, res: Response) => {
 	}
 }
 
+const downloadSnapshots = async (req: Request, res: Response) => {
+	const { id } = req.params
+	const user: User = res.locals.user
+
+	if (user.empires[0].id !== Number(id)) {
+		return res.status(500).json({ error: 'Empire ID mismatch' })
+	}
+	console.log('download...')
+
+	try {
+		const snapshot = await EmpireSnapshot.find({
+			where: {
+				e_id: id,
+			},
+			order: {
+				createdAt: 'ASC',
+			},
+		})
+
+		const parser = new Parser()
+
+		const csv = parser.parse(snapshot)
+
+		const readStream = new Readable()
+		readStream.push(csv)
+		readStream.push(null)
+
+		res.setHeader(
+			'Content-disposition',
+			'attachment; filename=downloaded-file.csv'
+		)
+		res.setHeader('Content-type', 'text/csv')
+
+		readStream.pipe(res)
+	} catch (error) {
+		console.log(error)
+		return res.status(500).json(error)
+	}
+}
+
 const router = Router()
 
 router.get('/:id', user, auth, getSnapshot)
 router.get('/:id/recent', user, auth, getRecentSnapshot)
 router.post('/:id/paginate', user, auth, paginateSnapshot)
+router.get('/:id/download', user, auth, downloadSnapshots)
 
 export default router
